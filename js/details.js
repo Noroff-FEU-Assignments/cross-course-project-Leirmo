0e1e8eebd905bafa2fca0321f7ebc51698aab6c5
Leirmo <103224025+Leirmo@users.noreply.github.com>
2023-12-26 16:57:52 +0100

    save

---
+++ js/details.js
@@ -0,0 +1,39 @@
+const detailContainer = document.querySelector(".container");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+console.log(id);
+
+const url = `https://www.game-hub.project-archive.no/wp-json/wc/store/products/${id}`;
+
+console.log(url);
+
+async function fetchGame() {
+  try {
+    const response = await fetch(url);
+    const details = await response.json();
+
+    console.log(details);
+
+    createHtml(details);
+  } catch (error) {
+    console.log(error);
+    detailContainer.innerHTML = message("error", error);
+  }
+}
+
+fetchGame();
+
+function createHtml(details) {
+  detailContainer.innerHTML = `<h1 class="game_title">${details.name}</h1>
+                                <div class="game_title">${details.description}</div>
+                                <img class="cover" src="${details.images[0].src}">
+                                <button type="button" class="cta_button cta_add_cart">
+            Pre-order
+          </button>
+`;
+}
